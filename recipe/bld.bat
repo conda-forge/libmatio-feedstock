18f65a70831abd01cda6218f9604178668403352
Ali Khodabakhsh <ali.khodabakhsh@ntnu.no>
2018-01-25 23:14:19 +0100

    add bld

---
+++ recipe/bld.bat
@@ -0,0 +1,2 @@
+appveyor DownloadFile https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.0/bin/windows/extra/hdf5-1.10.0-win"%ARCH%"-VS2015-shared.zip
+7z x -y hdf5-1.8.13-win%ARCH%-VS2010-shared.zip
