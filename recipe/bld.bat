102fcb54b1d2a597b78348ec65f687971068cb9a
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2018-01-25 23:59:51 +0100

    Use .sln 2008 for 2008

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,12 @@
 cd %SRC_DIR%
 
-rem set HDF5V=1.10.0
-rem appveyor DownloadFile https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-%HDF5V%/bin/windows/extra/hdf5-%HDF5V%-win%ARCH%-VS2015-shared.zip
-rem 7z x -y hdf5-%HDF5V%-win%ARCH%-VS2015-shared.zip
-rem msiexec /a %SRC_DIR%\hdf5\hdf5-%HDF5V%-win%ARCH%.msi /qb TARGETDIR=%SRC_DIR%\hdf5\msi
+if "%VS_YEAR%" == "2008" (
+    set SLN_FILE="visual_studio\matio_vs2008.sln"
+) else (
+    set SLN_FILE="visual_studio\matio.sln"
+)
 
-rem set HDF5_DIR=%SRC_DIR%\hdf5\msi\HDF_Group\HDF5\%HDF5V%
-
-set SLN_FILE=%SRC_DIR%\visual_studio\matio.sln
-set SLN_CFG=Release
-if "%ARCH%"=="32" (set SLN_PLAT=Win32) else (set SLN_PLAT=x64)
-
-REM Build step
-msbuild "%SLN_FILE%" /p:Configuration=%SLN_CFG%,Platform=%SLN_PLAT%,PlatformToolset=v140
+msbuild %SLN_FILE% /verbosity:normal
 if errorlevel 1 exit 1
 
 copy %SRC_DIR%\visual_studio\Release\libmatio.dll %LIBRARY_BIN%\
@@ -20,3 +14,4 @@ if errorlevel 1 exit 1
 
 copy %SRC_DIR%\visual_studio\Release\libmatio.lib %LIBRARY_LIB%\
 if errorlevel 1 exit 1
+
