a5e5b1b678bf4e08b5b7133fa8a6e95452fc6b77
Ali Khodabakhsh <ali.khodabakhsh@ntnu.no>
2018-01-26 13:39:37 +0100

    COMM

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,36 +1,27 @@
 :: Configure
 set CONF=Release
-if "%ARCH%" == "64" (
-  set SLN_PLAT=x64
-) else (
+if "%ARCH%" == "32" (
   set SLN_PLAT=Win32
+) else (
+  set SLN_PLAT=x64
 )
 
-if "%VS_YEAR%" == "2008" (
-  set TOOLSET=v90
-  set SLN_FILE="visual_studio\matio_vs2008.sln"
-)
-if "%VS_YEAR%" == "2015" (
-  set TOOLSET=v140
-  set SLN_FILE="visual_studio\matio.sln"
-)
-if "%VS_YEAR%" == "" (
-  echo Unknown VS version
-  exit 1
-)
+set TOOLSET=v140
+set SLN_FILE="visual_studio\matio.sln"
 
-call devenv %SLN_FILE% /Upgrade
+echo %LIBRARY_PREFIX%
 
 :: Build
 msbuild "%SLN_FILE%" ^
   /p:Configuration=Release ^
   /p:Platform=%SLN_PLAT% ^
-  /p:PlatformToolset=%TOOLSET%
+  /p:PlatformToolset=%TOOLSET% ^
+  /property:HDF5_DIR=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
 
 :: Install
-copy %SRC_DIR%\visual_studio\Release\libmatio.dll %LIBRARY_BIN%\
+copy %SRC_DIR%\visual_studio\%SLN_PLAT%\Release\libmatio.dll %LIBRARY_BIN%\
 if errorlevel 1 exit 1
-copy %SRC_DIR%\visual_studio\Release\libmatio.lib %LIBRARY_LIB%\
+copy %SRC_DIR%\visual_studio\%SLN_PLAT%\Release\libmatio.lib %LIBRARY_LIB%\
 if errorlevel 1 exit 1
 
