842f810a05a712d08bb619ff36fb39b76545303e
Silvio Traversaro <silvio@traversaro.it>
2021-08-02 17:40:31 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,22 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
 
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+  # See https://github.com/tbeu/matio/issues/78#issuecomment-357797381
+  MATIO_CROSS_COMPILATION_CONFIGURE_OPTS="--ac_cv_va_copy=C99"
+else
+  MATIO_CROSS_COMPILATION_CONFIGURE_OPTS=""
+fi
+
+
+
 ./configure \
   --enable-mat73 \
   --with-pic \
   --enable-extended-sparse \
   --prefix="${PREFIX}" \
   --with-zlib="${PREFIX}" \
-  --with-hdf5="${PREFIX}"
+  --with-hdf5="${PREFIX}" ${MATIO_CROSS_COMPILATION_CONFIGURE_OPTS}
 make
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
